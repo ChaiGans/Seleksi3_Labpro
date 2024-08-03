357159cde80a99087020e0160b7fa65994187752
FOS-Intern <elbertchailes888@gmail.com>
2024-08-03 16:02:31 +0700

    init

---
+++ src/main/java/seleksi/labpro/owca/OwcaApplication.java
@@ -0,0 +1,13 @@
+package seleksi.labpro.owca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seleksi/labpro/owca/OwcaApplicationTests.java
@@ -0,0 +1,13 @@
+package seleksi.labpro.owca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
