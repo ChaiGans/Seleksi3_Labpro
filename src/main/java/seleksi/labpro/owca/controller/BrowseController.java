2c78478f1f201841433c9823f8f22d935da3bd59
FOS-Intern <elbertchailes888@gmail.com>
2024-08-11 23:11:17 +0700

    feat: pagination

--- src/main/java/seleksi/labpro/owca/controller/BrowseController.java
+++ src/main/java/seleksi/labpro/owca/controller/BrowseController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import seleksi.labpro.owca.dto.FilmDto;
 import seleksi.labpro.owca.entity.Film;
 import seleksi.labpro.owca.entity.User;
@@ -31,22 +32,30 @@ public class BrowseController {
     }
 
     @GetMapping("/browse")
-    public String showBrowsePage(Model model, HttpServletRequest request) {
+    public String showBrowsePage(Model model, HttpServletRequest request, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "4") int size) {
         List<Film> allFilms = filmService.getAllFilms();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
+            return "redirect:/login";
         }
 
         String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return "redirect:/login";
+        }
+
         String token = authorizationHeader.substring(7);
 
         String userAuthEmail = jwtService.extractUsername(token);
 
         User loginUser = userService.findByEmail(userAuthEmail);
 
-        List<FilmDto> filmDTOs = allFilms.stream().map(film -> new FilmDto(
+        int totalFilms = allFilms.size();
+        int fromIndex = page * size;
+        int toIndex = Math.min(fromIndex + size, totalFilms);
+
+        List<FilmDto> filmDTOs = allFilms.subList(fromIndex, toIndex).stream().map(film -> new FilmDto(
                 Math.toIntExact(film.getId()),
                 film.getCoverImageUrl(),
                 film.getDescription(),
@@ -58,7 +67,12 @@ public class BrowseController {
                 film.getPrice(),
                 filmService.isBought(film.getId(), loginUser)
         )).collect(Collectors.toList());
+
         model.addAttribute("films", filmDTOs);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("size", size);
+        model.addAttribute("totalPages", (totalFilms + size - 1) / size);
+
         return "pages/browse.html";
     }
 }
